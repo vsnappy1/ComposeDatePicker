3cbd5d4e6acf4c30956d457b3805c62048d74701
Vishal Kumar <vsnappy1@gmail.com>
2023-05-03 00:59:52 -0500

    added infinity scroll and fixed some minor issues in scrolling

--- composeCalendar/src/main/java/com/vsnappy1/composecalendar/ui/model/CalendarUiState.kt
+++ composeCalendar/src/main/java/com/vsnappy1/composecalendar/ui/model/CalendarUiState.kt
@@ -5,12 +5,12 @@ import com.vsnappy1.composecalendar.data.model.Month
 import java.util.Calendar
 
 data class CalendarUiState(
-    val selectedYear: Int = Calendar.getInstance().get(Calendar.YEAR),
+    val selectedYear: Int = Calendar.getInstance()[Calendar.YEAR],
     val selectedYearIndex: Int = Constant.years.size / 2,
-    val availableMonths: List<Month> = Constant.getMonths(selectedYear),
-    val selectedMonth: Month = availableMonths[Calendar.getInstance().get(Calendar.MONTH)],
+    val selectedMonth: Month = Constant.getMonths(selectedYear)[Calendar.getInstance()[Calendar.MONTH]],
+    val selectedMonthIndex: Int = 36 + selectedMonth.number,
     val currentVisibleMonth: Month = selectedMonth,
-    val selectedDayOfMonth: Int? = Calendar.getInstance().get(Calendar.DAY_OF_MONTH),
+    val selectedDayOfMonth: Int? = Calendar.getInstance()[Calendar.DAY_OF_MONTH],
     val availableYears: List<Int> = Constant.years,
     val isMonthYearViewVisible: Boolean = true
 )
