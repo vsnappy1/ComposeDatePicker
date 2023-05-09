2fc848861b8f65ca5459bd4431072d01183ed614
Vishal Kumar <vsnappy1@gmail.com>
2023-05-08 23:31:57 -0500

    month and year scrolling improved

--- composeDatePicker/src/main/java/com/vsnappy1/datepicker/ui/Component.kt
+++ composeDatePicker/src/main/java/com/vsnappy1/datepicker/ui/Component.kt
@@ -6,7 +6,6 @@ import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.interaction.DragInteraction
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
@@ -19,7 +18,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
-import kotlinx.coroutines.delay
 
 
 @Composable
@@ -48,28 +46,22 @@ fun SwipeLazyColumn(
     listState: LazyListState,
     content: LazyListScope.() -> Unit
 ) {
-    var dragStarted by remember { mutableStateOf(false) }
+    var lastSelectedIndex by remember { mutableStateOf(0) }
+    var count by remember { mutableStateOf(0) }
 
     LaunchedEffect(key1 = Unit) {
         listState.scrollToItem(selectedIndex)
     }
-    if(!isAutoScrolling){
+    if (!isAutoScrolling) {
         LaunchedEffect(key1 = listState.firstVisibleItemScrollOffset) {
-            onSelectedIndexChange(listState.firstVisibleItemIndex + if (listState.firstVisibleItemScrollOffset > height.value / numberOfRowsDisplayed) 1 else 0)
-        }
-        LaunchedEffect(key1 = dragStarted) {
-            listState.animateScrollToItem(selectedIndex)
-        }
-
-        LaunchedEffect(key1 = Unit) {
-            listState.interactionSource.interactions.collect {
-                if (it is DragInteraction.Stop) {
-                    delay(100)
-                    dragStarted = false
-                } else if (it is DragInteraction.Start) {
-                    dragStarted = true
-                }
+            val index =
+                listState.firstVisibleItemIndex + if (listState.firstVisibleItemScrollOffset > height.value / numberOfRowsDisplayed) 1 else 0
+            onSelectedIndexChange(index)
+            count = if (index == lastSelectedIndex) ++count else 0
+            if (count >= 5) {
+                listState.animateScrollToItem(selectedIndex)
             }
+            lastSelectedIndex = index
         }
     }
 
